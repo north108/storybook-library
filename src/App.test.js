0cf442325c46c9ad917b1330d7b2569e4c86465f
north108 <elizabethjnorthrop@gmail.com>
2021-09-26 22:48:40 -0700

    Set up testing

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
-import { render, screen } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
+
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+// import 'jsdom-global/register';
+// import Adapter from 'enzyme-adapter-react-16';
+
+import React from 'react';
+import { shallow, mount , configure} from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('A suite', () => {
+  it('validate basic structure', () => {
+    const wrapper = shallow(<App />)
+    expect(wrapper.find('a').text()).toEqual('Learn React');
+  })
 });
+
